692f69a6f0ecdb738a4196b05a2f239841ea67fb
Mateus Trindade <teus718@gmail.com>
2024-07-04 16:56:25 -0300

    Front-End in development

---
+++ aplicação/public/js/main.js
@@ -0,0 +1,10 @@
+// Função para abrir o model 
+function mostrarModal(){
+    document.getElementById('modalBg').style.display = 'flex';
+}
+
+// Função para fechar o model
+function fecharModal(){
+    document.getElementById('modalBg').style.display = 'none';
+}
+
